99ab1aaaa36f2088f2cf936b2b969e9dfb6dfdbd
Mount-Blanc <mountblanc87@gmail.com>
2022-07-02 10:16:35 -0700

    creates a brand new url link

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const multer = require('multer')
 const mongoose = require("mongoose")
 const bcrypt = require ("bcrpy")
+const File = require('./models/File')
 
 const express = require('express')
 const app = express()
@@ -15,15 +16,22 @@ app.get('/', (req,res) => {
     res.render('index')
 })
 
-app.post('/upload', upload.single("file"), (req,res) => {
+app.post('/upload', upload.single("file"), async (req,res) => {
     const fileData = {
         path: req.file.path,
         originalName: req.file.originalname
     }
 
     if ( req.bpdy.password != null && req.body.password !==) {
-        fileData.password = bcrypt.hash
+        fileData.password = await bcrypt.hash(req.body.password, 10)
     }
+
+    const file = await File.create(fileData)
+    res.render('index', {fileLink:`${req.headers.origin}/file/${file.id}` })
 })
+
+app.get('/file/:id'), (req, res) => {
+
+}
 
 app.listen(process.env.PORT)
